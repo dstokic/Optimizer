b524e6cf54723dc7a5f025feaca271638313b745
Abena Laast <salaast@gmail.com>
2022-05-19 14:31:30 -0500

    get very basic preferences page up

--- src/components/Preferences.js
+++ src/components/Preferences.js
@@ -3,17 +3,18 @@ import Goal from './Goal'
 import './Preferences.css'
 
 const Preferences = () => {
-    const [data, setData] = useState([]);
-    useEffect(() => {
-        fetch('http://localhost:8080/api/movies')
-        .then(response => response.json())
-        .then(json => setData(json))
-    }, [])
+    const goals = ["Travel", "Save", "Invest"];
+    // const [goals, setGoals] = useState([]);
+    // useEffect(() => {
+    //     fetch('http://localhost:8080/api/movies')
+    //     .then(response => response.json())
+    //     .then(json => setGoals(json))
+    // }, [])
     return (
         <div>
             <h1 className="title">Choose a Goal</h1>
             <div className="goals">
-                {data.map((goal) => (
+                {goals.map((goal) => (
                     <Goal name={goal.name}/>
                 ))}
             </div>
