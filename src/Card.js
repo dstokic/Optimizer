da9941689d76e1061495615e0c7dc873b8dbe38d
Abena Laast <salaast@gmail.com>
2022-05-19 17:57:45 -0500

    update Card.js inputs

--- src/Card.js
+++ src/Card.js
@@ -2,16 +2,19 @@ import React from 'react'
 import './Card.css'
 
 const Card = ({id, name, image, bank, 
-    forStudents, lowInterest, goodCashBack, 
+    forStudents, lowInterest, hasCashBack, 
     annualFee, minCreditScore, forTravel}) => {
     return (
         <div className="card">
             <img src={image}></img>
             <p className="cardName">{name}</p>
-            <p className="cardBank">{brand}</p>
+            <p className="cardBank">{bank}</p>
+            <p className="cardForStudents">{forStudents}</p>
+            <p className="cardLowInterest">{lowInterest}</p>
+            <p className="cardHasCashBack">{hasCashBack}</p>
             <p className="cardFee">{annualFee}</p>
-            <p className="cardMinScore">{creditScoreReq}</p>
-            <p className="cardBenefits">{benefits}</p>
+            <p className="cardMinScore">{minCreditScore}</p>
+            <p className="cardForTravel">{forTravel}</p>
         </div>
     )
 }
