39324e4d77388a4746056d65e019f284f48deb18
Abena Laast <salaast@gmail.com>
2022-05-19 14:47:42 -0500

    add buttons with names from goals array

--- src/components/Preferences.js
+++ src/components/Preferences.js
@@ -4,18 +4,13 @@ import './Preferences.css'
 
 const Preferences = () => {
     const goals = ["Travel", "Save", "Invest"];
-    // const [goals, setGoals] = useState([]);
-    // useEffect(() => {
-    //     fetch('http://localhost:8080/api/movies')
-    //     .then(response => response.json())
-    //     .then(json => setGoals(json))
-    // }, [])
+
     return (
         <div>
             <h1 className="title">Choose a Goal</h1>
             <div className="goals">
                 {goals.map((goal) => (
-                    <Goal name={goal.name}/>
+                    <Goal name={goal}/>
                 ))}
             </div>
         </div>
