ab2f4e0a0bd5c245288423dcd5ce014a88f5b575
Abena Laast <salaast@gmail.com>
2022-05-19 19:13:36 -0500

    edit json files, add yarn.lock, edit questions, add card filter, start initializer for sample data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,131 @@
-import logo from './logo.svg';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-import Preferences from './components/Preferences';
-import Results from './components/Results';
-import NavBar from './components/NavBar';
-import './App.css';
+import React, { useState } from 'react';
+import Card from './Card';
 
-function App() {
-  return (
-    <div className="App">
-      <style>{'body { background-color: #f3f9ed; }'}</style>
-      <Router>
-        <NavBar/>
-        <Routes>
-          <Route path="/" element= {<Preferences/>}></Route>
-          <Route path="/results" element= {<Results/>}></Route>
-        </Routes>
-      </Router>
-    </div>
-  );
+export default function App() {
+	const questions = [
+		{
+			questionText: 'What is your main goal?',
+			answerOptions: [
+				{ answerText: 'Save Money', caterTo: ['lowInterest', true]},
+				{ answerText: 'Build Credit', caterTo: []},
+				{ answerText: 'Earn Rewards', caterTo: []},
+			],
+		},
+		{
+			questionText: 'Where does your credit score fall into?',
+			answerOptions: [
+				{ answerText: 'No Credit Score', caterTo: ['minCreditScore', 0]},
+				{ answerText: '-499', caterTo: ['minCreditScore', 499]},
+				{ answerText: '500-699', caterTo: ['minCreditScore', 699]},
+				{ answerText: '700-', caterTo: ['minCreditScore', 800]},
+			],
+		},
+		{
+			questionText: 'Is this your first credit card?',
+			answerOptions: [
+				{ answerText: 'Yes', isCorrect: [] },
+				{ answerText: 'No', isCorrect: [] },
+			],
+		},
+		{
+			questionText: 'Are you a student?',
+			answerOptions: [
+				{ answerText: 'Yes', caterTo: ['forStudents', true]},
+				{ answerText: 'No', caterTo: []},
+			],
+		},
+    {
+			questionText: 'Choose a bank you have established a history with.',
+			answerOptions: [
+				{ answerText: 'Bank1', caterTo: ['bank', 'Bank1']},
+        { answerText: 'Bank2', caterTo: ['bank', 'Bank2']},
+			],
+		},
+	];
+
+	const [currentQuestion, setCurrentQuestion] = useState(0);
+	const [showScore, setShowScore] = useState(false);
+  const [score, setScore] = useState(0);
+	var cards = [];
+
+  function initializeData() { // add cards to list of cards
+    cards.length = 0;
+    // Card 1, for a student
+    cards.push(new Card({
+      name: "Card1", image:"", bank:"Bank1", 
+      forStudents:true, lowInterest:true, hasCashBack:false, 
+      annualFee:0,minCreditScore:0, forTravel:false
+    }));
+
+    cards.push(new Card({
+      name: "Card2", image:"", bank:"Bank2", 
+      forStudents:false, lowInterest:true, hasCashBack:true, 
+      annualFee:50,minCreditScore:400, forTravel:false
+    }));
+
+    cards.push(new Card({
+      name: "Card3", image:"", bank:"Bank2", 
+      forStudents:false, lowInterest:false, hasCashBack:true, 
+      annualFee:150,minCreditScore:690, forTravel:true
+    }));
+
+  }
+
+  function filterCards(card, prop, val) {
+    switch(prop) {
+      case 'bank': cards = cards.filter(card.bank === val); return;
+      case 'forStudents': cards = cards.filter(card.forStudents); return;
+      case 'lowInterest': cards = cards.filter(card.lowInterest); return;
+      case 'hasCashBack': cards = cards.filter(card.hasCashBack); return;
+      case 'annualFee': cards = cards.filter(card.annualFee <= val); return;
+      case 'minCreditScore': cards = cards.filter(card.minCreditScore <= val); return;
+      case 'cardForTravel': cards = cards.filter(card.forTravel); return;
+      default: return;
+    }
+  }
+
+	const handleAnswerOptionClick = (caterTo) => {
+    setScore(0);
+		// for each card in cards, filter
+    let i = 0;
+    while (i < cards.length) { // go through cards
+      let card = cards[i];
+      // go through properties to compare
+      for (let propertyIdx = 0; propertyIdx < caterTo.length; propertyIdx += 1) {
+        let property = caterTo[propertyIdx][0];
+        let value = caterTo[propertyIdx][1];
+        filterCards(card, property, value);
+      }
+    }
+		const nextQuestion = currentQuestion + 1;
+
+		if (nextQuestion < questions.length) {
+			setCurrentQuestion(nextQuestion);
+		} else {
+			setShowScore(true);
+		}
+	};
+	return (
+		<div className='app'>
+			{showScore ? (
+				<div className='score-section'>
+					You scored {score} out of {questions.length}
+				</div>
+			) : (
+				<>
+					<div className='question-section'>
+						<div className='question-count'>
+							<span>Question {currentQuestion + 1}</span>/{questions.length}
+						</div>
+						<div className='question-text'>{questions[currentQuestion].questionText}</div>
+					</div>
+					<div className='answer-section'>
+						{questions[currentQuestion].answerOptions.map((answerOption) => (
+							<button onClick={() => handleAnswerOptionClick(answerOption.caterTo)}>{answerOption.answerText}</button>
+						))}
+					</div>
+				</>
+			)}
+		</div>
+	);
 }
-
-export default App;
--- src/Card.js
+++ src/Card.js
@@ -1,19 +1,19 @@
 import React from 'react'
 import './Card.css'
 
-const Card = ({id, name, image, bank, 
+const Card = ({id, name, bank, 
     forStudents, lowInterest, hasCashBack, 
     annualFee, minCreditScore, forTravel}) => {
     return (
         <div className="card">
-            <img src={image}></img>
+           
             <p className="cardName">{name}</p>
             <p className="cardBank">{bank}</p>
             <p className="cardForStudents">{forStudents}</p>
             <p className="cardLowInterest">{lowInterest}</p>
             <p className="cardHasCashBack">{hasCashBack}</p>
             <p className="cardFee">{annualFee}</p>
-            <p className="cardMinScore">{minCreditScore}</p>
+            <p className="cardMinCreditScore">{minCreditScore}</p>
             <p className="cardForTravel">{forTravel}</p>
         </div>
     )
